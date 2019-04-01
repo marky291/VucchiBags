e371d3b6920ca9e0ac6e23590f4163a60c0a8f96
Mark Hester <marky360@live.ie>
2019-04-01 19:33:01 +0100

    Entities Customer & Reservation with unique ID generation

---
+++ VucchiBags/Entity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VucchiBags
+{
+    abstract class Entity
+    {
+        /**
+         * The unique identifier for the entity
+         * 
+         * @var integer
+         */
+        protected int ID { get; set; }
+
+        /**
+         * Construct the entity
+         */
+        public Entity()
+        {
+            this.ID = this.GenerateIdentificationKey();
+        }
+
+        /**
+         * Generate an unique ID for the entity.
+         */
+        protected virtual int GenerateIdentificationKey()
+        {
+            return Math.Abs(Guid.NewGuid().GetHashCode());
+        }
+    }
+}
---
+++ VucchiBags/Reservations/Reservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VucchiBags.Customers;
+using VucchiBags.Rentals;
+
+namespace VucchiBags.Reservations
+{
+    class Reservation : Entity
+    {
+        public ICustomer Customer { get; set; }
+
+        public decimal DailyRate { get; set; }
+
+        public IRental Rental { get; set; }
+
+        public DateTime ETA { get; set; }
+
+        public DateTime ETT { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public int DiscountPercent { get; set; }
+    }
+}
